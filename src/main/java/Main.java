f80d3fba38818957747ef2e9666d08446cb79981
Nikolay Bienko <bikolya@gmail.com>
2014-04-26 12:40:44 +0400

    Simple auth page added

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import frontend.Frontend;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import javax.servlet.Servlet;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Servlet frontend = new Frontend();
+        Server server = new Server(8080);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(frontend), "/*");
+
+        server.start();
+        server.join();
+    }
+}
