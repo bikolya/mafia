28986df1f3a5d00367a67d7395f01797f29e9e2c
Nikolay Bienko <bikolya@gmail.com>
2014-04-26 19:06:57 +0400

    Fake auth added, need fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import frontend.Frontend;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
@@ -7,12 +10,20 @@ import javax.servlet.Servlet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+
         Servlet frontend = new Frontend();
         Server server = new Server(8080);
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         server.setHandler(context);
         context.addServlet(new ServletHolder(frontend), "/*");
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setResourceBase("static");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+        server.setHandler(handlers);
 
         server.start();
         server.join();
--- src/main/java/frontend/Frontend.java
+++ src/main/java/frontend/Frontend.java
@@ -6,32 +6,105 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Frontend extends HttpServlet {
 
     private String login = "";
+    private AtomicLong userIdGenerator = new AtomicLong();
+
+
+    public static String getTime()
+    {
+        Date date = new Date();
+        date.getTime();
+        DateFormat formatter = new SimpleDateFormat("HH.mm.ss");
+        return formatter.format(date);
+    }
+
+    private Long getUserId(HttpSession session)
+    {
+        Long userId = (Long) session.getAttribute("userId");
+        if (userId == null) {
+            userId = userIdGenerator.getAndIncrement();
+            session.setAttribute("userId", userId);
+        }
+        return userId;
+    }
+
+    private boolean checkAuth(String login, String pass)
+    {
+
+        String hadrUser = "1";
+        String hardPass = "1";
+        String hardUser2 = "2";
+        String hardPass2 = "2";
+        return (login.equals(hadrUser) && pass.equals(hardPass))
+                || (login.equals(hardUser2) && pass.equals(hardPass2) );
+    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        login = request.getParameter("login");
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         Map<String, Object> pageVariables = new HashMap<>();
-        pageVariables.put("lastLogin", login == null ? "" : login);
-        response.getWriter().println(PageGenerator.getPage("authform.tml", pageVariables));
+
+        String path = request.getPathInfo();
+        switch (path) {
+            case "/userid":
+                Long userId = getUserId(request.getSession());
+                pageVariables.put("refreshPeriod", "1000");
+                pageVariables.put("serverTime", getTime());
+                pageVariables.put("userId", userId);
+                response.getWriter().println(PageGenerator.getPage("userid.tml", pageVariables));
+                return;
+        }
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        login = request.getParameter("login");
+        String login = request.getParameter("login");
+        String pass = request.getParameter("password");
+
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
-        Map<String, Object> pageVariables = new HashMap<>();
-        pageVariables.put("lastLogin", login == null ? "" : login);
-        response.getWriter().println(PageGenerator.getPage("authform.tml", pageVariables));
+        HttpSession currentSession = request.getSession();
+        String path = request.getPathInfo();
+
+        switch (path) {
+            case "/login":
+                if (checkAuth(login, pass)) {
+                    if (checkUserId(currentSession)) {
+                        closeUserId(currentSession);
+                    }
+                    openUserId(currentSession);
+                    response.sendRedirect("/userid");
+                } else
+                    response.sendRedirect("/");
+                break;
+        }
+    }
+
+    private boolean checkUserId(HttpSession session)
+    {
+        return session.getAttribute("userId") != null;
+    }
+
+    private void openUserId(HttpSession session)
+    {
+        session.setAttribute("userId", userIdGenerator.getAndIncrement());
+    }
+
+    private void closeUserId(HttpSession session)
+    {
+        session.setAttribute("userId", null);
     }
 }
 
